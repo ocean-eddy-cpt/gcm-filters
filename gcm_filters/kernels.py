9bef41bcad47be2d9fe88f1a018e2ba3b1afbafb
Ryan Abernathey <ryan.abernathey@gmail.com>
2020-12-06 10:22:42 -0500

    fix pre-commit stuff

--- gcm_filters/kernels.py
+++ gcm_filters/kernels.py
@@ -1,25 +1,41 @@
+"""
+Core smoothing routines that operate on 2D arrays.
+"""
+
 import functools
 
 
 try:
-    from cupy import get_array_module
+    from cupy import get_array_module as _get_array_module
 except ImportError:
     import numpy as np
 
-    def get_array_module(*args):
+    def _get_array_module(*args):
         return np
 
 
-def try_to_use_cupy(func):
+def _try_to_use_cupy(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        np = get_array_module(*args)
+        np = _get_array_module(*args)
         return func(*args, **kwargs)
+
     return wrapper
 
 
-@try_to_use_cupy
+@_try_to_use_cupy
 def simple_diffusion_kernel(phi):
+    """Classic diffusion stencil for regular grid.
+
+    Parameters
+    ----------
+    phi : array_like
+
+    Returns
+    -------
+    array_like
+        The diffusive tendency for `phi`
+    """
     return (
         -4 * phi
         + np.roll(phi, -1, axis=-1)
