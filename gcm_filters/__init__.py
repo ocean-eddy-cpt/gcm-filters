e0070b043cee4a92933d4f3063e8222965721bb0
Ryan Abernathey <ryan.abernathey@gmail.com>
2020-12-06 20:25:55 -0500

    remove unused module

--- gcm_filters/__init__.py
+++ gcm_filters/__init__.py
@@ -1,14 +1,6 @@
 """
-ioos_pkg_skeleton is not a reak package, just a set of best practices examples.
+Diffusion-based smoothers for coarse-graining GCM data.
 """
-
-from .gcm_filters import meaning_of_life, meaning_of_life_url
-
-
-__all__ = [
-    "meaning_of_life",
-    "meaning_of_life_url",
-]
 
 try:
     from ._version import __version__
