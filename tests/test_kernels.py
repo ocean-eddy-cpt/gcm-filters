8639a29fc7b9753eea1f839545fc18d3fcf44280
NoraLoose <nora.loose@gmail.com>
2021-02-19 12:25:49 -0700

    add tripolar grid tests

--- tests/test_kernels.py
+++ tests/test_kernels.py
@@ -1,4 +1,5 @@
 import enum
+import copy
 
 import numpy as np
 import pytest
@@ -27,17 +28,13 @@ def grid_type_field_and_extra_kwargs(request):
         extra_kwargs["dxs"] = grid_data
         extra_kwargs["dys"] = grid_data
         extra_kwargs["area"] = grid_data
-    if grid_type == GridType.POP_TRIPOLAR_GRID:
+    if grid_type == GridType.POP_SIMPLE_TRIPOLAR_GRID:
         mask_data = np.ones_like(data)
         mask_data[3 * (ny // 4) :, (nx // 4) : (nx // 2)] = 0
-        mask_data[
-            0, :
-        ] = 0  #  we need Antarctic land to disable periodic boundary condition in y-direction
+        mask_data[0, :] = 0  #  Antarctica
         extra_kwargs["wet_mask"] = mask_data
+        extra_kwargs["position"] = "T"
     return grid_type, data, extra_kwargs
-
-
-# TODO: implement check (elsewhere) that makes sure that sourthernmost row of user-specified land mask is land
 
 
 def test_conservation(grid_type_field_and_extra_kwargs):
@@ -54,43 +51,116 @@ def test_required_grid_vars(grid_type_field_and_extra_kwargs):
     assert set(grid_vars) == set(extra_kwargs)
 
 
+################## Tripolar grid tests ##############################################
+def fold_northern_boundary(ufield, nx, invert):
+    folded = ufield[-1, :]  # grab northernmost row
+    folded = folded[::-1]  # mirror it
+    if invert:
+        folded = -folded
+    folded = np.roll(folded, -1)  # shift by 1 cell to the left
+    ufield[-1, 0 : nx // 2] = folded[0 : nx // 2]
+    ufield[-1, nx // 2 - 1] = 0  # pivot point (first Arctic singularity) is on land
+    ufield[-2, nx // 2 - 1] = 0  # pivot point (first Arctic singularity) is on land
+    ufield[-1, -1] = 0  # second Arctic singularity is on land too
+    # ufield[-2,-1] = 0 # second Arctic singularity is on land too
+    return ufield
+
+
 @pytest.fixture(
     scope="module",
-    params=[
-        member
-        for name, member in GridType.__members__.items()
-        if name == "POP_TRIPOLAR_GRID"
-    ],
+    params=[("POP_SIMPLE_TRIPOLAR_GRID", "T"), ("POP_SIMPLE_TRIPOLAR_GRID", "U")],
 )
-def grid_type_field_and_extra_kwargs_and_delta_function(request):
-    grid_type = request.param
+def tripolar_grid_field_and_extra_kwargs(request):
+    grid_type = GridType.__members__[request.param[0]]
+    position = request.param[1]
+
     ny, nx = (128, 256)
-    data = np.zeros((ny, nx))
-    delta_pos = nx // 5
-    data[-1, delta_pos] = 1  # delta function at northern boundary
+    data = np.random.rand(ny, nx)
+
     extra_kwargs = {}
+    extra_kwargs["position"] = position
+
     mask_data = np.ones_like(data)
+    # mask_data[3 * (ny // 4) :, (nx // 4) : (nx // 2)] = 0
+    mask_data[: (ny // 2), : (nx // 2)] = 0
+    mask_data[0, :] = 0  #  Antarctica
+
+    if position == "U":
+        mask_data = fold_northern_boundary(mask_data, nx, invert=False)
+        data = fold_northern_boundary(data, nx, invert=True)
+
     extra_kwargs["wet_mask"] = mask_data
 
-    return grid_type, data, extra_kwargs, nx, delta_pos
+    return grid_type, data, extra_kwargs
 
 
-def test_tripolar_exchanges(grid_type_field_and_extra_kwargs_and_delta_function):
-    """This test checks whether a delta function gets diffused isotropically across the northern boundary seam line of a tripolar grid. """
+def test_for_antarctica(tripolar_grid_field_and_extra_kwargs):
+    """This test checks that we get an error if southernmost row of wet_mask has entry not equal to zero. """
+    grid_type, _, extra_kwargs = tripolar_grid_field_and_extra_kwargs
 
-    (
-        grid_type,
-        data,
-        extra_kwargs,
-        nx,
-        delta_pos,
-    ) = grid_type_field_and_extra_kwargs_and_delta_function
-    print(grid_type)
+    nx = np.shape(extra_kwargs["wet_mask"])[1]
+    random_loc = np.random.randint(0, nx)
+    bad_kwargs = copy.deepcopy(extra_kwargs)
+    bad_kwargs["wet_mask"][0, random_loc] = 1
+
+    LaplacianClass = ALL_KERNELS[grid_type]
+    with pytest.raises(AssertionError, match=r"Wet mask requires .*"):
+        laplacian = LaplacianClass(**bad_kwargs)
+
+
+def test_northern_boundary_fold(tripolar_grid_field_and_extra_kwargs):
+    """This test checks that wet get an error if uppermost row of wet_mask or data does not fold onto itself. """
+    grid_type, data, extra_kwargs = tripolar_grid_field_and_extra_kwargs
+    if extra_kwargs["position"] == "U":
+        LaplacianClass = ALL_KERNELS[grid_type]
+        laplacian = LaplacianClass(**extra_kwargs)
+
+        nx = np.shape(extra_kwargs["wet_mask"])[1]
+        bad_data = data.copy()
+        bad_data[-1, : nx // 2] = 1
+        bad_data[-1, nx // 2 :] = 0
+        with pytest.raises(AssertionError, match=r"Uppermost row of input field .*"):
+            diffused = laplacian(bad_data)
+
+        bad_kwargs = copy.deepcopy(extra_kwargs)
+        bad_kwargs["wet_mask"][-1, : nx // 2] = 1
+        bad_kwargs["wet_mask"][-1, nx // 2 :] = 0
+        with pytest.raises(AssertionError, match=r"Uppermost row of wet mask .*"):
+            laplacian = LaplacianClass(**bad_kwargs)
+
+
+def test_tripolar_exchanges(tripolar_grid_field_and_extra_kwargs):
+    """This test checks that Laplacian exchanges across northern boundary seam line of tripolar grid are correct. """
+    grid_type, data, extra_kwargs = tripolar_grid_field_and_extra_kwargs
     LaplacianClass = ALL_KERNELS[grid_type]
     laplacian = LaplacianClass(**extra_kwargs)
-    diffused = laplacian(data)
 
-    np.testing.assert_allclose(
-        diffused[-2, delta_pos], diffused[-1, nx - delta_pos - 1], atol=1e-12
-    )
-    # this test is only appropriate for tracer fields.
+    delta_fct = np.zeros_like(data)
+    nx = np.shape(delta_fct)[1]
+
+    position = extra_kwargs["position"]
+    if position == "T":
+        random_loc = np.random.randint(0, nx)
+        delta_fct[-1, random_loc] = 1  # deploy mass at northern boundary
+        diffused = laplacian(delta_fct)
+        # check that delta function gets diffused isotropically across northern boundary
+        np.testing.assert_allclose(
+            diffused[-2, random_loc], diffused[-1, nx - random_loc - 1], atol=1e-12
+        )
+
+    elif position == "U":
+        random_loc = np.random.randint(0, nx // 2 - 1)
+        delta_fct[
+            -2, random_loc
+        ] = 1  # deploy mass just below northern boundary on the left side
+        diffused = laplacian(delta_fct)
+        # check that delta function gets diffused isotropically across northern boundary
+        np.testing.assert_allclose(
+            diffused[-3, random_loc], -diffused[-1, nx - random_loc - 2], atol=1e-12
+        )
+        # check that northernmost row of diffused data folds onto itself
+        diffused = laplacian(data)
+        folded = diffused[-1, :]  # grab northernmost row
+        folded = -folded[::-1]  # mirror and invert it
+        folded = np.roll(folded, -1)  # shift by 1 cell to the left
+        np.testing.assert_allclose(diffused[-1, :], folded, atol=1e-12)
