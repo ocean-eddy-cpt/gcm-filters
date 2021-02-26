f4e2fb7dfd87a3b448854fb8f4bda8b3f068972e
Arthur <arthur.guillaumin@gmail.com>
2021-02-26 13:02:50 -0500

    Change conservation test to be area-weighted

--- tests/test_kernels.py
+++ tests/test_kernels.py
@@ -11,7 +11,7 @@ def grid_type_field_and_extra_kwargs(request):
     ny, nx = (128, 256)
     data = np.random.rand(ny, nx)
     mask_data = np.ones_like(data)
-    mask_data[: (ny // 2), : (nx // 2)] = 0
+    # mask_data[: (ny // 2), : (nx // 2)] = 0
 
     extra_kwargs = {}
     if grid_type == GridType.CARTESIAN_WITH_LAND:
@@ -47,7 +47,11 @@ def test_conservation(grid_type_field_and_extra_kwargs):
     grid_type, data, extra_kwargs = grid_type_field_and_extra_kwargs
     LaplacianClass = ALL_KERNELS[grid_type]
     laplacian = LaplacianClass(**extra_kwargs)
-    res = laplacian(data)
+    for k, v in extra_kwargs.items():
+        if k.startswith("area"):
+            areas = v
+            break
+    res = laplacian(data) * areas
     np.testing.assert_allclose(res.sum(), 0.0, atol=1e-12)
 
 
