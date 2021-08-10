900a5ab180745473e07102467e75f6474d8b0a5d
Ryan Abernathey <ryan.abernathey@gmail.com>
2021-08-10 12:59:10 +0200

    move fixtures to conftest

--- tests/test_kernels.py
+++ tests/test_kernels.py
@@ -1,7 +1,5 @@
 import copy
 
-from typing import Tuple
-
 import numpy as np
 import pytest
 
@@ -14,91 +12,6 @@ from gcm_filters.kernels import (
     GridType,
     required_grid_vars,
 )
-
-
-# define (for now: hard-code) which grids are associated with vector Laplacians
-vector_grids = [gt for gt in GridType if gt.name in {"VECTOR_C_GRID"}]
-# all remaining grids are for scalar Laplacians
-scalar_grids = [gt for gt in GridType if gt not in vector_grids]
-
-
-_RANDOM_SEED = 42
-rng = Generator(PCG64(_RANDOM_SEED))
-
-_grid_kwargs = {
-    GridType.REGULAR: [],
-    GridType.REGULAR_AREA_WEIGHTED: ["area"],
-    GridType.REGULAR_WITH_LAND: ["wet_mask"],
-    GridType.REGULAR_WITH_LAND_AREA_WEIGHTED: ["wet_mask", "area"],
-    GridType.IRREGULAR_WITH_LAND: [
-        "wet_mask",
-        "dxw",
-        "dyw",
-        "dxs",
-        "dys",
-        "area",
-        "kappa_w",
-        "kappa_s",
-    ],
-    GridType.TRIPOLAR_REGULAR_WITH_LAND_AREA_WEIGHTED: ["wet_mask", "area"],
-    GridType.TRIPOLAR_POP_WITH_LAND: ["wet_mask", "dxe", "dye", "dxn", "dyn", "tarea"],
-}
-
-
-def _make_random_data(shape: Tuple[int, int]) -> np.ndarray:
-    return rng.random(shape)
-
-
-def _make_mask_data(shape: Tuple[int, int]) -> np.ndarray:
-    mask_data = np.ones(shape)
-    ny, nx = shape
-    mask_data[0, :] = 0  #  Antarctica; required for some kernels
-    mask_data[: (ny // 2), : (nx // 2)] = 0
-    return mask_data
-
-
-def _make_irregular_grid_data(shape: Tuple[int, int]) -> np.ndarray:
-    # avoid large-amplitude variation, ensure positive values, mean of 1
-    grid_data = 0.9 + 0.2 * rng.random(shape)
-    assert np.all(grid_data > 0)
-    return grid_data
-
-
-def _make_irregular_tripole_grid_data(shape: Tuple[int, int]) -> np.ndarray:
-    # avoid large-amplitude variation, ensure positive values, mean of 1
-    grid_data = 0.9 + 0.2 * rng.random(shape)
-    assert np.all(grid_data > 0)
-    # make northern edge grid data fold onto itself
-    nx = shape[-1]
-    half_northern_edge = grid_data[-1, : (nx // 2)]
-    grid_data[-1, (nx // 2) :] = half_northern_edge[::-1]
-    return grid_data
-
-
-################## Scalar Laplacian tests ##############################################
-@pytest.fixture(scope="module", params=scalar_grids)
-def grid_type_field_and_extra_kwargs(request):
-    grid_type = request.param
-    shape = (128, 256)
-
-    data = _make_random_data(shape)
-
-    extra_kwargs = {}
-    for name in _grid_kwargs[grid_type]:
-        if name == "wet_mask":
-            extra_kwargs[name] = _make_mask_data(shape)
-        elif "kappa" in name:
-            extra_kwargs[name] = np.ones(shape)
-        else:
-            extra_kwargs[name] = _make_irregular_grid_data(shape)
-
-    # northern edge grid data has to fold onto itself for tripole grids
-    if grid_type == GridType.TRIPOLAR_POP_WITH_LAND:
-        for name in _grid_kwargs[grid_type]:
-            if name in ["dxn", "dyn"]:
-                extra_kwargs[name] = _make_irregular_tripole_grid_data(shape)
-
-    return grid_type, data, extra_kwargs
 
 
 def test_conservation(grid_type_field_and_extra_kwargs):
@@ -220,41 +133,20 @@ def test_flux(grid_type_field_and_extra_kwargs, direction):
 
 
 ################## Tripolar grid tests for scalar Laplacians ##############################################
-tripolar_grids = [gt for gt in GridType if gt.name.startswith("TRIPOLAR")]
-
-
-@pytest.fixture(scope="module", params=tripolar_grids)
-# the following test data mirrors a regular grid because
-# these are the assumptions of test_tripolar_exchanges
-def tripolar_grid_type_field_and_extra_kwargs(request):
-    grid_type = request.param
-    shape = (128, 256)
-
-    data = _make_random_data(shape)
-
-    extra_kwargs = {}
-    for name in _grid_kwargs[grid_type]:
-        if name == "wet_mask":
-            extra_kwargs[name] = _make_mask_data(shape)
-        else:
-            extra_kwargs[name] = np.ones_like(data)
-
-    return grid_type, data, extra_kwargs
 
 
 def test_for_antarctica(tripolar_grid_type_field_and_extra_kwargs):
     """This test checks that we get an error if southernmost row of wet_mask has entry not equal to zero."""
     grid_type, _, extra_kwargs = tripolar_grid_type_field_and_extra_kwargs
 
-    if grid_type in tripolar_grids:
-        nx = np.shape(extra_kwargs["wet_mask"])[1]
-        random_loc = rng.integers(0, nx)
-        bad_kwargs = copy.deepcopy(extra_kwargs)
-        bad_kwargs["wet_mask"][0, random_loc] = 1
+    nx = np.shape(extra_kwargs["wet_mask"])[1]
+    random_loc = 10
+    bad_kwargs = copy.deepcopy(extra_kwargs)
+    bad_kwargs["wet_mask"][0, random_loc] = 1
 
-        LaplacianClass = ALL_KERNELS[grid_type]
-        with pytest.raises(AssertionError, match=r"Wet mask requires .*"):
-            laplacian = LaplacianClass(**bad_kwargs)
+    LaplacianClass = ALL_KERNELS[grid_type]
+    with pytest.raises(AssertionError, match=r"Wet mask requires .*"):
+        laplacian = LaplacianClass(**bad_kwargs)
 
 
 def test_folding_of_northern_gridedge_data(tripolar_grid_type_field_and_extra_kwargs):
@@ -282,90 +174,27 @@ def test_tripolar_exchanges(tripolar_grid_type_field_and_extra_kwargs):
     """This test checks that Laplacian exchanges across northern boundary seam line of tripolar grid are correct."""
     grid_type, data, extra_kwargs = tripolar_grid_type_field_and_extra_kwargs
 
-    if grid_type in tripolar_grids:
-        LaplacianClass = ALL_KERNELS[grid_type]
-        laplacian = LaplacianClass(**extra_kwargs)
+    LaplacianClass = ALL_KERNELS[grid_type]
+    laplacian = LaplacianClass(**extra_kwargs)
 
-        delta = np.zeros_like(data)
-        nx = np.shape(delta)[1]
-        # deploy mass at northern boundary, away from boundaries and pivot point in middle
-        random_loc = rng.integers(1, nx // 2 - 2)
-        delta[-1, random_loc] = 1
+    delta = np.zeros_like(data)
+    nx = np.shape(delta)[1]
+    # deploy mass at northern boundary, away from boundaries and pivot point in middle
+    random_loc = 10
+    delta[-1, random_loc] = 1
 
-        regular_kwargs = copy.deepcopy(extra_kwargs)
-        regular_kwargs["wet_mask"][0, random_loc] = 1
+    regular_kwargs = copy.deepcopy(extra_kwargs)
+    regular_kwargs["wet_mask"][0, random_loc] = 1
 
-        diffused = laplacian(delta)
-        # check that delta function gets diffused isotropically across northern boundary;
-        # this assumes regular grid data in fixture
-        np.testing.assert_allclose(
-            diffused[-2, random_loc], diffused[-1, nx - random_loc - 1], atol=1e-12
-        )
+    diffused = laplacian(delta)
+    # check that delta function gets diffused isotropically across northern boundary;
+    # this assumes regular grid data in fixture
+    np.testing.assert_allclose(
+        diffused[-2, random_loc], diffused[-1, nx - random_loc - 1], atol=1e-12
+    )
 
 
 #################### Vector Laplacian tests ########################################
-
-
-@pytest.fixture(scope="module", params=vector_grids)
-def vector_grid_type_field_and_extra_kwargs(request):
-    grid_type = request.param
-    ny, nx = (128, 256)
-
-    extra_kwargs = {}
-    if grid_type == GridType.VECTOR_C_GRID:
-        # construct spherical coordinate system similar to MOM6 NeverWorld2 grid
-        # define latitudes and longitudes
-        lat_min = -70
-        lat_max = 70
-        lat_u = np.linspace(
-            lat_min + 0.5 * (lat_max - lat_min) / ny,
-            lat_max - 0.5 * (lat_max - lat_min) / ny,
-            ny,
-        )
-        lat_v = np.linspace(lat_min + (lat_max - lat_min) / ny, lat_max, ny)
-        lon_min = 0
-        lon_max = 60
-        lon_u = np.linspace(lon_min + (lon_max - lon_min) / nx, lon_max, nx)
-        lon_v = np.linspace(
-            lon_min + 0.5 * (lon_max - lon_min) / nx,
-            lon_max - 0.5 * (lon_max - lon_min) / nx,
-            nx,
-        )
-        (geolon_u, geolat_u) = np.meshgrid(lon_u, lat_u)
-        (geolon_v, geolat_v) = np.meshgrid(lon_v, lat_v)
-        # radius of a random planet smaller than Earth
-        R = 6378000 * rng.random((1,))
-        # dx varies spatially
-        extra_kwargs["dxCu"] = R * np.cos(geolat_u / 360 * 2 * np.pi)
-        extra_kwargs["dxCv"] = R * np.cos(geolat_v / 360 * 2 * np.pi)
-        extra_kwargs["dxBu"] = extra_kwargs["dxCv"] + np.roll(
-            extra_kwargs["dxCv"], -1, axis=1
-        )
-        extra_kwargs["dxT"] = extra_kwargs["dxCu"] + np.roll(
-            extra_kwargs["dxCu"], 1, axis=1
-        )
-        # dy is set constant, equal to dx at the equator
-        dy = np.max(extra_kwargs["dxCu"]) * np.ones((ny, nx))
-        extra_kwargs["dyCu"] = dy
-        extra_kwargs["dyCv"] = dy
-        extra_kwargs["dyBu"] = dy
-        extra_kwargs["dyT"] = dy
-        # compute grid cell areas
-        extra_kwargs["area_u"] = extra_kwargs["dxCu"] * extra_kwargs["dyCu"]
-        extra_kwargs["area_v"] = extra_kwargs["dxCv"] * extra_kwargs["dyCv"]
-        # set isotropic and anisotropic kappas
-        extra_kwargs["kappa_iso"] = np.ones((ny, nx))
-        extra_kwargs["kappa_aniso"] = np.ones((ny, nx))
-        # put a big island in the middle
-        mask_data = np.ones((ny, nx))
-        mask_data[: (ny // 2), : (nx // 2)] = 0
-        extra_kwargs["wet_mask_t"] = mask_data
-        extra_kwargs["wet_mask_q"] = mask_data
-
-    data_u = rng.random((ny, nx))
-    data_v = rng.random((ny, nx))
-
-    return grid_type, data_u, data_v, extra_kwargs, geolat_u
 
 
 def test_conservation_under_solid_body_rotation(
