8ca345afc4660be8ae6ec9380c67ce8f23af3f8f
NoraLoose <nora.loose@gmail.com>
2021-09-10 11:34:45 +0200

    Add MOM5 Laplacians to test suite

--- gcm_filters/kernels.py
+++ gcm_filters/kernels.py
@@ -13,13 +13,13 @@ from .gpu_compat import ArrayType, get_array_module
 GridType = enum.Enum(
     "GridType",
     [
-        "MOM5U",
-        "MOM5T",
         "REGULAR",
         "REGULAR_AREA_WEIGHTED",
         "REGULAR_WITH_LAND",
         "REGULAR_WITH_LAND_AREA_WEIGHTED",
         "IRREGULAR_WITH_LAND",
+        "MOM5U",
+        "MOM5T",
         "TRIPOLAR_REGULAR_WITH_LAND_AREA_WEIGHTED",
         "TRIPOLAR_POP_WITH_LAND",
         "VECTOR_C_GRID",
@@ -140,95 +140,6 @@ class RegularLaplacianWithArea(AreaWeightedMixin, RegularLaplacian):
 
 
 ALL_KERNELS[GridType.REGULAR_AREA_WEIGHTED] = RegularLaplacianWithArea
-
-
-@dataclass
-class MOM5LaplacianU(BaseLaplacian):
-    """Laplacian for MOM5 gird (velocity points)"""
-
-    dxt: ArrayType
-    dyt: ArrayType
-    dxu: ArrayType
-    dyu: ArrayType
-    area_u: ArrayType
-    wet: ArrayType
-
-    def __post_init__(self):
-        np = get_array_module(self.wet)
-
-        self.x_wet_mask = self.wet * np.roll(self.wet, -1, axis=-1)
-        self.y_wet_mask = self.wet * np.roll(self.wet, -1, axis=-2)
-
-    def __call__(self, field: ArrayType):
-        """Uses code by Elizabeth"""
-        np = get_array_module()
-        field = np.nan_to_num(field)
-        fx = 2 * (np.roll(field, shift=-1, axis=-2) - field)
-        fx /= np.roll(self.dxt, -1, axis=-2) + np.roll(self.dxt, (-1, -1), axis=(0, 1))
-        fy = 2 * (np.roll(field, shift=-1, axis=-1) - field)
-        fy /= np.roll(self.dyt, -1, axis=-1) + np.roll(self.dyt, (-1, -1), axis=(0, 1))
-        fx *= self.x_wet_mask
-        fy *= self.y_wet_mask
-
-        out1 = 0.5 * fx * (self.dyu + np.roll(self.dyu, -1, axis=-2))
-        out1 -= (
-            0.5 * np.roll(fx, 1, axis=-2) * (self.dyu + np.roll(self.dyu, 1, axis=-2))
-        )
-        out1 /= self.area_u
-
-        out2 = 0.5 * fy * (self.dxu + np.roll(self.dxu, -1, axis=-1))
-        out2 -= (
-            0.5 * np.roll(fy, 1, axis=-1) * (self.dxu + np.roll(self.dxu, 1, axis=-1))
-        )
-        out2 /= self.area_u
-        return out1 + out2
-
-
-ALL_KERNELS[GridType.MOM5U] = MOM5LaplacianU
-
-
-@dataclass
-class MOM5LaplacianT(BaseLaplacian):
-    """Laplacian for MOM5 grid (tracer points)."""
-
-    dxt: ArrayType
-    dyt: ArrayType
-    dxu: ArrayType
-    dyu: ArrayType
-    area_t: ArrayType
-    wet: ArrayType
-
-    def __post_init__(self):
-        np = get_array_module(self.wet)
-
-        self.x_wet_mask = self.wet * np.roll(self.wet, -1, axis=-1)
-        self.y_wet_mask = self.wet * np.roll(self.wet, -1, axis=-2)
-
-    def __call__(self, field):
-        np = get_array_module(field)
-        field = np.nan_to_num(field)
-        fx = 2 * (np.roll(field, -1, axis=-2) - field)
-        fx /= self.dxu + np.roll(self.dxu, 1, axis=-1)
-        fy = 2 * (np.roll(field, -1, axis=-1) - field)
-        fy /= self.dyu + np.roll(self.dyu, 1, axis=-2)
-        fx *= self.x_wet_mask
-        fy *= self.y_wet_mask
-
-        out1 = fx * 0.5 * (self.dyt + np.roll(self.dyt, -1, axis=-2))
-        out1 -= (
-            np.roll(fx, 1, axis=-2) * 0.5 * (self.dyt + np.roll(self.dyt, 1, axis=-2))
-        )
-        out1 /= self.area_t
-
-        out2 = fy * 0.5 * (self.dxt + np.roll(self.dxt, -1, axis=-1))
-        out2 -= (
-            np.roll(fy, 1, axis=-1) * 0.5 * (self.dxt + np.roll(self.dxt, 1, axis=-1))
-        )
-        out2 /= self.area_t
-        return out1 + out2
-
-
-ALL_KERNELS[GridType.MOM5T] = MOM5LaplacianT
 
 
 @dataclass
@@ -391,6 +302,95 @@ class IrregularLaplacianWithLandMask(BaseScalarLaplacian):
 
 
 ALL_KERNELS[GridType.IRREGULAR_WITH_LAND] = IrregularLaplacianWithLandMask
+
+
+@dataclass
+class MOM5LaplacianU(BaseScalarLaplacian):
+    """Laplacian for MOM5 gird (velocity points)"""
+
+    wet_mask: ArrayType
+    dxt: ArrayType
+    dyt: ArrayType
+    dxu: ArrayType
+    dyu: ArrayType
+    area_u: ArrayType
+
+    def __post_init__(self):
+        np = get_array_module(self.wet_mask)
+
+        self.x_wet_mask = self.wet_mask * np.roll(self.wet_mask, -1, axis=-1)
+        self.y_wet_mask = self.wet_mask * np.roll(self.wet_mask, -1, axis=-2)
+
+    def __call__(self, field: ArrayType):
+        """Uses code by Elizabeth"""
+        np = get_array_module()
+        field = np.nan_to_num(field)
+        fx = 2 * (np.roll(field, shift=-1, axis=-2) - field)
+        fx /= np.roll(self.dxt, -1, axis=-2) + np.roll(self.dxt, (-1, -1), axis=(0, 1))
+        fy = 2 * (np.roll(field, shift=-1, axis=-1) - field)
+        fy /= np.roll(self.dyt, -1, axis=-1) + np.roll(self.dyt, (-1, -1), axis=(0, 1))
+        fx *= self.x_wet_mask
+        fy *= self.y_wet_mask
+
+        out1 = 0.5 * fx * (self.dyu + np.roll(self.dyu, -1, axis=-2))
+        out1 -= (
+            0.5 * np.roll(fx, 1, axis=-2) * (self.dyu + np.roll(self.dyu, 1, axis=-2))
+        )
+        out1 /= self.area_u
+
+        out2 = 0.5 * fy * (self.dxu + np.roll(self.dxu, -1, axis=-1))
+        out2 -= (
+            0.5 * np.roll(fy, 1, axis=-1) * (self.dxu + np.roll(self.dxu, 1, axis=-1))
+        )
+        out2 /= self.area_u
+        return out1 + out2
+
+
+ALL_KERNELS[GridType.MOM5U] = MOM5LaplacianU
+
+
+@dataclass
+class MOM5LaplacianT(BaseScalarLaplacian):
+    """Laplacian for MOM5 grid (tracer points)."""
+
+    wet_mask: ArrayType
+    dxt: ArrayType
+    dyt: ArrayType
+    dxu: ArrayType
+    dyu: ArrayType
+    area_t: ArrayType
+
+    def __post_init__(self):
+        np = get_array_module(self.wet_mask)
+
+        self.x_wet_mask = self.wet_mask * np.roll(self.wet_mask, -1, axis=-1)
+        self.y_wet_mask = self.wet_mask * np.roll(self.wet_mask, -1, axis=-2)
+
+    def __call__(self, field):
+        np = get_array_module(field)
+        field = np.nan_to_num(field)
+        fx = 2 * (np.roll(field, -1, axis=-2) - field)
+        fx /= self.dxu + np.roll(self.dxu, 1, axis=-1)
+        fy = 2 * (np.roll(field, -1, axis=-1) - field)
+        fy /= self.dyu + np.roll(self.dyu, 1, axis=-2)
+        fx *= self.x_wet_mask
+        fy *= self.y_wet_mask
+
+        out1 = fx * 0.5 * (self.dyt + np.roll(self.dyt, -1, axis=-2))
+        out1 -= (
+            np.roll(fx, 1, axis=-2) * 0.5 * (self.dyt + np.roll(self.dyt, 1, axis=-2))
+        )
+        out1 /= self.area_t
+
+        out2 = fy * 0.5 * (self.dxt + np.roll(self.dxt, -1, axis=-1))
+        out2 -= (
+            np.roll(fy, 1, axis=-1) * 0.5 * (self.dxt + np.roll(self.dxt, 1, axis=-1))
+        )
+        out2 /= self.area_t
+        return out1 + out2
+
+
+ALL_KERNELS[GridType.MOM5T] = MOM5LaplacianT
 
 
 @dataclass
